d68f0bb76f0c784666999e06040cec44465cd189
Brooks_Wilcox <brookswill88@gmail.com>
2021-10-20 22:50:41 -0600

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+;
+
+function destructivelyAppendCat(name){
+    // const allCats = [...cats]
+    const allCats = cats.push(name)
+    return allCats;
+}
+
+function destructivelyPrependCat(name){
+    const allCats = cats.unshift(name)
+    return allCats;
+}
+
+function destructivelyRemoveLastCat(){
+    const allCats = cats.pop();
+    return allCats;
+}
+
+function destructivelyRemoveFirstCat(){
+    const allCats = cats.shift();
+    return allCats;
+    
+}
+
+function appendCat(name){
+    const allCats = [...cats]
+    allCats.push(name);
+    return allCats;
+}
+
+function prependCat(name){
+    const allCats = [...cats];
+    allCats.unshift(name);
+    return allCats;
+}
+
+function removeLastCat(){
+    const allCats = [...cats];
+    allCats.pop();
+    return allCats;
+}
+
+function removeFirstCat(){
+    const allCats = [...cats];
+    allCats.shift();
+    return allCats;
+}
